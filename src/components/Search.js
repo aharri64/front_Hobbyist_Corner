8d82e52a73f84c59d7750189784f88a3ad37486f
scott95100 <scott95100@gmail.com>
2021-03-09 09:52:52 -0700

    add search title

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,7 +15,7 @@ class Search extends  React.Component {
 		return (
 			<div className="container">
 				{/*Heading*/}
-				<h2 className="heading">Live Search: React Application</h2>
+				<h2 className="heading">Search: All Projects</h2>
 				{/*Search Input*/}
 				<label className="search-label" htmlFor="search-input">
 					<input
