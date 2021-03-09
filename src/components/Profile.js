d187f5f3bb36a2148ce9956a3d0667db63e4c2de
leov1963 <leov1963@gmail.com>
2021-03-08 21:40:52 -0800

    [components] Profile.js: Fix indentation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Profile = (props) => {
-   const { handleLogout, user } = props;
-   const { id, name, email, exp } = user;
+    const { handleLogout, user } = props;
+    const { id, name, email, exp } = user;
    const expirationTime = new Date(exp * 1000);
-   let currentTime = Date.now();
+    let currentTime = Date.now();
 
    // make a condition that compares exp and current time
-   if (currentTime >= expirationTime) {
-       handleLogout();
-       alert('Session has ended. Please login to continue.');
-   }
+    if (currentTime >= expirationTime) {
+        handleLogout();
+        alert('Session has ended. Please login to continue.');
+    }
 
-   const userData = user ?
-   (<div>
-       <h1>Profile</h1>
-       <p>Name: {name}</p>
-       <p>Email: {email}</p>
-       <p>ID: {id}</p>
-   </div>) : <h2>Loading...</h2>
+    const userData = user ?
+    (<div>
+        <h1>Profile</h1>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>ID: {id}</p>
+    </div>) : <h2>Loading...</h2>
 
     const errorDiv = () => {
         return (
