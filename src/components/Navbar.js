0fc86b4b599bbe458459bcf80de1470f426c1eb7
leov1963 <leov1963@gmail.com>
2021-03-11 15:49:39 -0800

    [components] Navbar.js: Add logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
+import '../nav.css'
+
+import logo from '../assets/logo.png';
+
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <Link className="navbar-brand" to="/">H. C.</Link>
+                <Link className="navbar-brand" to="/"><img className="logo
+                " src={logo} alt="H.C.-Logo" height="80px" /></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="#navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
