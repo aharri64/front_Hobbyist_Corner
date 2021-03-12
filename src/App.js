7e4cf0295b61a1154ac63a5607908a40c575a988
aharri64 <haveacompleteday@gmail.com>
2021-03-12 14:59:19 -0700

    checking on config

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import setAuthToken from './utils/setAuthToken';
 
 // CSS
 import './App.css';
+const REACT_APP_SERVER_URL= process.env.REACT_APP_SERVER_URL
+console.log(REACT_APP_SERVER_URL)
 
 // Components
 import Signup from './components/Signup';
