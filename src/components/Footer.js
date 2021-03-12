126b0fabc0f5b746bd64d65d2e991fb395bd68bb
leov1963 <leov1963@gmail.com>
2021-03-12 12:53:18 -0800

    Footer: Update content - Shorten last names so content fits on screen on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer bg-dark">
             <div className="container text-center">
-                <span className="text-muted">@2021 Built by <a href="https://github.com/aharri64">Amir Harrison</a>, <a href="https://github.com/leov1963">Leo Vincent</a> and <a href="https://github.com/scott95100">Scott Sherwood</a></span>
+                <span className="text-muted">@2021 Built by <a href="https://github.com/aharri64">Amir H,</a> <a href="https://github.com/leov1963">Leo V,</a> and <a href="https://github.com/scott95100">Scott S.</a></span>
             </div>
         </footer>
     )
