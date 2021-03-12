4509149891b048960681984756e925f1bcdb1df0
leov1963 <leov1963@gmail.com>
2021-03-11 16:36:28 -0800

    [components] About.js: Update Class Names

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import '../About.css';
+
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>This is about us...</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            <h1 className="about-title text-color">About</h1>
+            <h4 className="about-sub-title text-color">This is about us...</h4>
+            <p className="about-content text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Tempore dolorum excepturi, pariatur dolore fuga perferendis 
                 mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
                 dolorem tempora possimus accusantium, fugit eius quidem?
