2af4b7f0b367455984a8d568c49ce279fd186af9
leov1963 <leov1963@gmail.com>
2021-03-15 08:47:33 -0700

    Update Welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,8 @@ import '../Welcome.css'
 const Welcome = () => {
     return (
         <div>
-            <h1 className="welcome-title text-color">Welcome</h1>
+            <h1 className="welcome-title text">Welcome to Hobbyiest Corner!</h1>
+            <h3 className="welcome-sub-title text"><a class="new-link" href="/signup">Create a profile</a> and share your projects with our community!</h3>
         </div>
     )
 }
